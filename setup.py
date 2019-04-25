10f8718c3c7cd1085021c9c8cced48f9b35de8e5
Philipp Kolberg <39984529+PKizzle@users.noreply.github.com>
2019-04-25 10:06:44 +0200

    Update setup.py for macOS usage

--- setup.py
+++ setup.py
@@ -1,14 +1,18 @@
 from distutils.core import setup, Extension
 import sys
+import platform
 
-module1 = Extension('_posixshmem',
+linux_module = Extension('_posixshmem',
                     define_macros = [('HAVE_SHM_OPEN', '1'), ('HAVE_SHM_UNLINK', '1'), ('HAVE_SYS_MMAN_H', '1')],
                     libraries = ['rt'],
+                    sources = ['posixshmem.c'])
+darwin_module = Extension('_posixshmem',
+                    define_macros = [('HAVE_SHM_OPEN', '1'), ('HAVE_SHM_UNLINK', '1'), ('HAVE_SYS_MMAN_H', '1')],
                     sources = ['posixshmem.c'])
 
 setup (name = 'Shared memory backport',
        version = '1.0',
        description = 'Simple backport of the multiprocessing.shared_memory module freshlay landed in python3.8',
        py_modules = ['shared_memory'],
-       ext_modules = [module1] if not sys.platform.startswith("win") else []
+       ext_modules = [linux_module] if platform.system() == 'Linux' else [darwin_module] if platform.system == 'Darwin' else []
        )
