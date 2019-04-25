6425b7ff0fc080dbf0d8f9c608ab1d48318cafb8
Philipp Kolberg <39984529+PKizzle@users.noreply.github.com>
2019-04-25 10:09:37 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,9 +10,9 @@ darwin_module = Extension('_posixshmem',
                     define_macros = [('HAVE_SHM_OPEN', '1'), ('HAVE_SHM_UNLINK', '1'), ('HAVE_SYS_MMAN_H', '1')],
                     sources = ['posixshmem.c'])
 
-setup (name = 'Shared memory backport',
-       version = '1.0',
-       description = 'Simple backport of the multiprocessing.shared_memory module freshlay landed in python3.8',
+setup (name = 'shared-memory-backport',
+       version = '1.1',
+       description = 'Simple backport of the multiprocessing.shared_memory module freshlay landed in python 3.8',
        py_modules = ['shared_memory'],
        ext_modules = [linux_module] if platform.system() == 'Linux' else [darwin_module] if platform.system == 'Darwin' else []
        )
